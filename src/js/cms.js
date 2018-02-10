cd7aee01533d808f92c1bf98745895edf33818c5
Ian <ianmilliken@live.co.uk>
2018-02-10 10:42:31 +1000

    Testing custom widget

--- src/js/cms.js
+++ src/js/cms.js
@@ -1,2 +1,38 @@
 import React from "react";
 import CMS from "netlify-cms";
+
+const DataSelectControl = createClass({
+	handleChange: function(e) {
+    this.props.onChange(e.target.value.split(',').map((e) => e.trim()));
+  },
+
+  render: function() {
+    var value = this.props.value;
+    return h('input', { type: 'text', value: value ? value.join(', ') : '', onChange: this.handleChange });
+  }
+});
+
+var CategoriesControl = createClass({
+  handleChange: function(e) {
+    this.props.onChange(e.target.value.split(',').map((e) => e.trim()));
+  },
+
+  render: function() {
+    var value = this.props.value;
+    return h('input', { type: 'text', value: value ? value.join(', ') : '', onChange: this.handleChange });
+  }
+});
+
+var CategoriesPreview = createClass({
+  render: function() {
+    return h('ul', {},
+      this.props.value.map(function(val, index) {
+        return h('li', {key: index}, val);
+      })
+    );
+  }
+});
+
+CMS.registerWidget('categories', CategoriesControl, CategoriesPreview);
+
+CMS.registerWidget('dataSelect', DataSelectControl);
