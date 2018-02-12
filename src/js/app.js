723e7ba1d5661f7c323f939c9c4b751a81b41666
Ian <ianmilliken@live.co.uk>
2018-02-13 08:46:35 +1000

    Handle login on invite

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,18 @@
 import ContactForm from './contactForm';
+//import SVGImageReplacer from './svgImageReplacer';
 
 // Internal scripts
 ContactForm();
+//SVGImageReplacer();
+
+
+// Better login handling
+if (window.netlifyIdentity) {
+  window.netlifyIdentity.on("init", user => {
+    if (!user) {
+      window.netlifyIdentity.on("login", () => {
+        document.location.href = "/admin/";
+      });
+    }
+  });
+}
