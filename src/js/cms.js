cd8a12b372b8dca5adb2de6520e68f2318aa545f
Ian <ianmilliken@live.co.uk>
2018-02-15 20:57:18 +1000

    Youtube widget, added cms preview & shortcode for use in markdown. Unfortunately widget will not work when placed in front matter, so will not work for case studies

--- src/js/cms.js
+++ src/js/cms.js
@@ -22,7 +22,7 @@ CMS.registerEditorComponent({
   // Fields the user need to fill out when adding an instance of the component
   fields: [{name: 'id', label: 'Youtube Video ID', widget: 'string'}],
   // Pattern to identify a block as being an instance of this component
-  pattern: /^youtube (\S+)$/,
+  pattern: /^{{<\s?youtube (\S+)\s?>}}$/,
   // Function to extract data elements from the regexp match
   fromBlock: function(match) {
     return {
@@ -31,13 +31,13 @@ CMS.registerEditorComponent({
   },
   // Function to create a text block from an instance of this component
   toBlock: function(obj) {
-    return 'youtube ' + obj.id;
+    return '{{< youtube ' + obj.id + ' >}}';
   },
   // Preview output for this component. Can either be a string or a React component
   // (component gives better render performance)
   toPreview: function(obj) {
     return (
-      '<img src="http://img.youtube.com/vi/' + obj.id + '/maxresdefault.jpg" alt="Youtube Video"/>'
+      '<div class="container image-wrapper"><img src="http://img.youtube.com/vi/' + obj.id + '/maxresdefault.jpg" alt="Youtube Video"/></div>'
     );
   }
 });
