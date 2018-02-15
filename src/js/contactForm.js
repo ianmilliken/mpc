2957c6eef70221522b0f50cbeeba31779e5b42fa
Ian <ianmilliken@live.co.uk>
2018-02-16 07:58:13 +1000

    Fix for form submission

--- src/js/contactForm.js
+++ src/js/contactForm.js
@@ -14,67 +14,70 @@ export default function contactForm() {
 
 	form.addEventListener('submit', function(e) {
 		e.preventDefault();
-		console.log("Form submitted");
 
-		// Get the email of the visitor
-		window._agile.set_email(email.value);
+		if (email.validity.valid && fullname.validity.valid && message.validity.valid) {
+			console.log("Form submitted");
 
-		// Create and send the contact
-		var contact = {};
-		contact.email = email.value;
-		contact.first_name = fullname.value.split(' ')[0];
-		contact.last_name = fullname.value.split(' ')[1];
-		contact.phone = phone.value;
-		contact.tags = "website contact form";
+			// Get the email of the visitor
+			window._agile.set_email(email.value);
 
-		window._agile.create_contact(contact, {
-			success: function (data) {
-				alert("Thanks, we'll be in touch!");
-				//console.log(data);
-				createAgileTask();
-			},
-			error: function (data) {
-				alert("Sorry, we could not complete your request!");
-				//console.log(data);
+			// Create and send the contact
+			var contact = {};
+			contact.email = email.value;
+			contact.first_name = fullname.value.split(' ')[0];
+			contact.last_name = fullname.value.split(' ')[1];
+			contact.phone = phone.value;
+			contact.tags = "website contact form";
+
+			window._agile.create_contact(contact, {
+				success: function (data) {
+					alert("Thanks, we'll be in touch!");
+					//console.log(data);
+					createAgileTask();
+				},
+				error: function (data) {
+					alert("Sorry, we could not complete your request!");
+					//console.log(data);
+				}
+			});
+
+			var createAgileTask = function() {
+				// Create and send task
+				var task = {};
+				task.type = "FOLLOW_UP";
+				task.priority_type = "HIGH";
+				task.subject = fullname.value.split(' ')[0] + ' ' + fullname.value.split(' ')[1] + ' made an enquiry on your website.';
+				task.due = tomorrow;
+
+				window._agile.add_task(task, {
+					success: function (data) {
+						//console.log("Success creating task");
+						//console.log(data);
+						createAgileNote();
+					},
+					error: function (data) {
+						console.log("Error creating task");
+						//console.log(data);
+					}
+				});
 			}
-		});
 
-		var createAgileTask = function() {
-			// Create and send task
-			var task = {};
-			task.type = "FOLLOW_UP";
-			task.priority_type = "HIGH";
-			task.subject = fullname.value.split(' ')[0] + ' ' + fullname.value.split(' ')[1] + ' made an enquiry on your website.';
-			task.due = tomorrow;
+			var createAgileNote = function() {
+				var note = {};
+				note.subject = fullname.value.split(' ')[0] + ' ' + fullname.value.split(' ')[1] + ' made an enquiry on your website.';
+				note.description = message.value;
 
-			window._agile.add_task(task, {
-				success: function (data) {
-					//console.log("Success creating task");
-					//console.log(data);
-					createAgileNote();
-				},
-				error: function (data) {
-					console.log("Error creating task");
-					//console.log(data);
-				}
-			});
-		}
-
-		var createAgileNote = function() {
-			var note = {};
-			note.subject = fullname.value.split(' ')[0] + ' ' + fullname.value.split(' ')[1] + ' made an enquiry on your website.';
-			note.description = message.value;
-
-			_agile.add_note(note, {
-				success: function (data) {
-					console.log("Success creating note");
-					//console.log(data);
-				},
-				error: function (data) {
-					console.log("Error creating note");
-					//console.log(data);
-				}
-			});
+				_agile.add_note(note, {
+					success: function (data) {
+						console.log("Success creating note");
+						//console.log(data);
+					},
+					error: function (data) {
+						console.log("Error creating note");
+						//console.log(data);
+					}
+				});
+			}
 		}
 	});
 
