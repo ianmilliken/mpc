b2aa09590a47c22539381c1053f715c01eeb8d3c
Ian <ianmilliken@live.co.uk>
2018-02-18 21:48:31 +1000

    [#78] Added dotenv. Removed placeholder agile intergration, using env variables now

--- webpack.conf.js
+++ webpack.conf.js
@@ -1,5 +1,8 @@
 import webpack from "webpack";
 import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export default {
   module: {
@@ -24,6 +27,10 @@ export default {
     new webpack.ProvidePlugin({
       // no longer allowed to omit the '-loader' suffix when using loaders
       "fetch": "imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch"
+    }),
+    new webpack.DefinePlugin({
+      CRM_ACCOUNT_NAME: JSON.stringify(process.env.CRM_ACCOUNT_NAME),
+      CRM_API_KEY: JSON.stringify(process.env.CRM_API_KEY)
     })
   ],
 
