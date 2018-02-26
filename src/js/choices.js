35416fa8451ca3fe418d82d4247e347c6f22a197
Ian <ianmilliken@live.co.uk>
2018-02-26 21:20:50 +1000

    Simpler landing page

--- src/js/choices.js
+++ src/js/choices.js
@@ -3,57 +3,6 @@ export default function Choices() {
 	console.log('Choices has loaded');
 	console.log('----------------------------------------');
 
-	var picked;
-
-	const choices = document.querySelectorAll('[data-choice]'),
-				results = document.querySelectorAll('[data-result]'),
-				steps = document.querySelectorAll('[data-choice-step]');
-
-	for (let i = 0; i < choices.length; i++) {
-		choices[i].addEventListener('click', function(e) {
-			e.preventDefault();
-			proceed(choices[i].dataset.choice);
-		})
-	}
-
-	function proceed(choice) {
-		//console.log(choice);
-		var resultArray = findMatchingResult(choice);
-		//console.log('Match is ::: ' + x)
-		picked = choice;
-		console.log(picked);
-		steps[0].classList.add('is-hidden');
-		steps[1].classList.remove('is-hidden');
-		for (let i = 0; i < resultArray.length; i++) {
-			resultArray[i].classList.remove('is-hidden');
-		}
-	}
-
-	function findMatchingResult(choice) {
-		var arr = [];
-		for (let i = 0; i < results.length; i++) {
-			if (results[i].dataset.result === choice) {
-				arr = arr.concat(results[i]);
-			}
-		}
-		return arr;
-	}
-
-	document.getElementById('choiceBack').addEventListener('click', function(e) {
-		e.preventDefault();
-		picked = undefined;
-		console.log(picked);
-		steps[0].classList.remove('is-hidden');
-		steps[1].classList.add('is-hidden');
-		for (let i = 0; i < results.length; i++) {
-			results[i].classList.add('is-hidden');
-		}
-	});
-
-
-
-
-
 	// Handle form submit
 	const form = document.getElementById('landingForm'),
 				email = document.getElementById('landingEmail'),
@@ -87,7 +36,7 @@ export default function Choices() {
 						alert("Thanks, we'll be in touch!");
 						//console.log(data);
 						createAgileTask();
-						document.querySelector('[data-download=' + picked + ']').click();
+						document.querySelector('[data-download]').click();
 					},
 					error: function (data) {
 						alert("Sorry, we could not complete your request!");
